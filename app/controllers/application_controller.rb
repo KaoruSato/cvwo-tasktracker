bdd6fbc976b752efdadf2bf524f702025190ff80
Daryl Chan <dckj95@gmail.com>
2018-12-29 17:25:44 +0800

    Fix CSRF protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
   rescue_from ActiveRecord::RecordInvalid, with: :render_bad_request_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 

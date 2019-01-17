b7e67dfc2cc425f3024b5a324bb28c7ad8f3c56e
Daryl Chan <dckj95@gmail.com>
2019-01-17 16:39:42 +0800

    Add document title indicator

--- app/javascript/src/components/Overview.jsx
+++ app/javascript/src/components/Overview.jsx
@@ -4,6 +4,11 @@ class Overview extends React.Component {
   render() {
     const outstanding = this.props.tasks.filter(t => !t.done).length;
 
+    // Set document title
+    if (outstanding > 0) {
+      document.title = `(${outstanding}) Task Tracker`;
+    }
+
     return (
       <div className="has-text-centered overview">
         <h1>
